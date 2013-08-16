649652128b7b92152afb3076c285a085e5a8701d
vgil <victor.gil.sepulveda@gmail.com>
2013-08-16 14:52:50 +0200

    First commit. Serial Scheduler has been changed.

---
+++ scheduler/scheduler/serialScheduler.py
@@ -0,0 +1,85 @@
+'''
+Created on 16/08/2012
+
+@author: victor
+'''
+class SerialTask(object):
+    def __init__(self, function, name, kwargs, description):
+        self.function = function
+        self.name = name
+        self.kwargs = kwargs
+    
+    def run(self):
+        return self.function(**(self.kwargs))
+    
+class SerialScheduler(object):
+    """
+    Not very efficient scheduler (it would be better to build a dependency tree).
+    """
+    
+    def __init__(self):
+        """
+        dependencies {"task_name":["dep_task_A", "dep_task_B"]}
+        """
+        self.tasks = []
+        self.dependencies = {}
+        self.not_completed = []
+        self.finished = []
+        self.results = []
+    
+    def run(self):
+        """
+        Tries to run all the tasks, checking for dependencies.
+        """
+        while len( self.not_completed) > 0:
+            #Choose an available process
+            task_name = self.choose_runnable_task()
+            
+            if task_name is None:
+                print "It was impossible to pick a suitable task for running. Check dependencies."
+                break;
+            else:
+                # Run a process
+                self.run_task(task_name)
+                self.remove_task(task_name)
+                # List processes
+                self.list_processes()
+        
+        self.ended()
+    
+    def choose_runnable_task(self):
+        for task_name in self.not_completed:
+            if not task_name in self.dependencies: # This process has no dependencies
+                return task_name;
+        return None # All task have dependencies (circular dependencies for instance)
+    
+    def run_task(self, task_name):
+        """
+        """
+        self.results.append(self.tasks[task_name].run())
+    
+    def remove_task(self, task_name):
+        """
+        
+        """
+        # Remove it from all dependencies. At the end all dependencies lists must be empty.
+        for tn in self.dependencies:
+            if task_name in self.dependencies[tn]:
+                self.dependencies[tn].remove(task_name)
+        
+        # Remove it from the not_completed list
+        self.not_completed.remove(task_name)
+        
+        # Add it to the list of completed tasks
+        self.finished.append(task_name)
+    
+    def add_task(self, task_name, target_function, function_kwargs, description):
+        if not task_name in self.tasks.keys():
+            task = SerialTask( name = task_name, description = description, function = target_function, kwargs=function_kwargs)
+            task.description = description
+            self.tasks.append(task)
+            self.not_completed.append(task_name)
+        else:
+            print "[Error SerialScheduler::add_task] Task %s already exists."%task_name
+            exit()
+        
---
+++ scheduler/scheduler/test/TestTaskSchedulers.py
@@ -0,0 +1,118 @@
+'''
+Created on 07/02/2013
+
+@author: victor
+'''
+import unittest
+import time
+
+def test_without_params():
+    time.sleep(1)
+
+def test_with_params(seconds):
+    time.sleep(seconds)
+    
+def add_processes(scheduler):
+    """"
+    Builds a dependency structure like this one:
+    
+        1    7  8
+       / \      |
+      2  3      9
+      | / \
+      4 5 6
+    """"
+    
+    scheduler.add_task()
+    
+class TestTaskSchedulers(unittest.TestCase):
+    
+    
+#     def test_ProcessParallelScheduler(self):
+#          
+#         # Without dependencies!
+#         pool = ProcessParallelScheduler(2, sleep_time = 1)
+#         pool.add_process("Process 1", "First process",  test_without_params, {})
+#         pool.add_process("Process 2", "Second process", test_with_params, {"seconds":4})
+#         pool.add_process("Process 3", "Third process",  test_with_params, {"seconds":3})
+#         pool.consume()
+#   
+#         expected = [{'action': '[Process Begin]', 'message': {'process': 'Process 1', 'description': 'First process'}}, 
+#                                         {'action': '[Process Begin]', 'message': {'process': 'Process 2', 'description': 'Second process'}}, 
+#                                         {'action': '[Process End]', 'message': {'process': 'Process 1', 'description': 'First process'}}, 
+#                                         {'action': '[Process Begin]', 'message': {'process': 'Process 3', 'description': 'Third process'}}, 
+#                                         {'action': '[Process End]', 'message': {'process': 'Process 2', 'description': 'Second process'}}, 
+#                                         {'action': '[Process End]', 'message': {'process': 'Process 3', 'description': 'Third process'}}] 
+#   
+#         self.assertItemsEqual(expected, pool.observer.messages["ProcessLauncher"])
+#           
+#         # And with dependencies!
+#         pool = ProcessParallelScheduler(2, sleep_time = 1)
+#         pool.add_process("Process 1", "First process", test_without_params, {})
+#         pool.add_process("Process 2", "Second process", test_with_params, {"seconds":3}, ["Process 1"])
+#         pool.add_process("Process 3", "Third process", test_with_params, {"seconds":2},  ["Process 1"])
+#         # This may oblige p1 to be executed and finished alone even if we have 2 slots
+#         pool.consume()
+#           
+#         expected = [{'action': '[Process Begin]', 'message': {'process': 'Process 1', 'description': 'First process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 1', 'description': 'First process'}}, 
+#                     {'action': '[Process Begin]', 'message': {'process': 'Process 2', 'description': 'Second process'}}, 
+#                     {'action': '[Process Begin]', 'message': {'process': 'Process 3', 'description': 'Third process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 2', 'description': 'Second process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 3', 'description': 'Third process'}}]
+#           
+#         self.assertItemsEqual(expected, pool.observer.messages["ProcessLauncher"])
+#           
+#         # With three slots all have to start
+#         pool = ProcessParallelScheduler(3, sleep_time = 1)
+#         pool.add_process("Process 1", "First process", test_without_params, {})
+#         pool.add_process("Process 2", "Second process", test_with_params, {"seconds":3} )
+#         pool.add_process("Process 3", "Third process", test_with_params, {"seconds":2} )
+#         # This may oblige p1 to be executed and finished alone even if we have 2 slots
+#         pool.consume()
+#          
+#         expected = [{'action': '[Process Begin]', 'message': {'process': 'Process 1', 'description': 'First process'}}, 
+#                     {'action': '[Process Begin]', 'message': {'process': 'Process 2', 'description': 'Second process'}}, 
+#                     {'action': '[Process Begin]', 'message': {'process': 'Process 3', 'description': 'Third process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 1', 'description': 'First process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 3', 'description': 'Third process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 2', 'description': 'Second process'}}]
+#  
+#          
+#         self.assertItemsEqual(expected, pool.observer.messages["ProcessLauncher"])
+#          
+#         # With three slots and dependencies, the behavior is exactly equal than the one having 2 slots
+#         pool = ProcessParallelScheduler(3, sleep_time = 1)
+#         pool.add_process("Process 1", "First process", test_without_params, {})
+#         pool.add_process("Process 2", "Second process", test_with_params, {"seconds":3}, ["Process 1"])
+#         pool.add_process("Process 3", "Third process", test_with_params, {"seconds":2},  ["Process 1"])
+#         # This may oblige p1 to be executed and finished alone even if we have 2 slots
+#         pool.consume()
+#           
+#         expected = [{'action': '[Process Begin]', 'message': {'process': 'Process 1', 'description': 'First process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 1', 'description': 'First process'}}, 
+#                     {'action': '[Process Begin]', 'message': {'process': 'Process 2', 'description': 'Second process'}}, 
+#                     {'action': '[Process Begin]', 'message': {'process': 'Process 3', 'description': 'Third process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 2', 'description': 'Second process'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 3', 'description': 'Third process'}}]
+#   
+#         self.assertItemsEqual(expected, pool.observer.messages["ProcessLauncher"])
+# 
+#     def test_SerialScheduler(self):
+#         pool = SerialScheduler(sleep_time = 1)
+#         pool.add_process("Process 1", "First process",  test_without_params, {})
+#         pool.add_process("Process 2", "Second process", test_with_params, {"seconds":4})
+#         pool.add_process("Process 3", "Third process",  test_with_params, {"seconds":3})
+#         pool.consume()
+# 
+#         expected = [{'action': '[Process Begin]', 'message': {'process': 'Process 1'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 1'}}, 
+#                     {'action': '[Process Begin]', 'message': {'process': 'Process 2'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 2'}},
+#                     {'action': '[Process Begin]', 'message': {'process': 'Process 3'}}, 
+#                     {'action': '[Process End]', 'message': {'process': 'Process 3'}}]
+#         self.assertItemsEqual(expected, pool.observer.messages["SerialProcess"])
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_ProcessParallelScheduler']
+    unittest.main()
