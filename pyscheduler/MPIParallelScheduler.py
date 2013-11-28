da80b810f9d113a862ddd4298894059d34b24a31
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-11-28 12:25:35 +0100

    Added comments to MPI scheduler.

--- pyscheduler/MPIParallelScheduler.py
+++ pyscheduler/MPIParallelScheduler.py
@@ -7,40 +7,50 @@ from mpi4py import MPI
 from pyscheduler.serialScheduler import SerialScheduler
 
 class MPIParallelScheduler(SerialScheduler):
-    
+    """
+    Implements an MPI version of the scheduler (powered by mpi4py). It is a gain a master-slave pattern
+    implementation, so one process is used to control the protocol.
+    """
+
     def __init__(self, share_results_with_all_processes = False, functions = {}):
+        """
+        Creates an instance of this scheduler.
+        @param share_results_with_all_processes: If true, it will broadcast the final results
+        array so that all the processes have a copy of it.
+        @param functions: @see SerialScheduler
+        """
         SerialScheduler.__init__(self,functions)
         self.comm = MPI.COMM_WORLD
         self.rank = self.comm.Get_rank()
         self.share_results_with_all_processes = share_results_with_all_processes
         self.number_of_processes = self.comm.Get_size()
-#         print self.number_of_processes, "processes"
         self.running = []
-    
-    
+
+
     def run(self):
         """
-        Tries to run all the tasks, checking for dependencies.
+        Tries to run all the tasks, checking for dependencies. In this case it uses an MPI scheme to handle
+        the processes.
+        The message protocol is the same as the one used in the ProcessParallelScheduler class.
         """
         self.function_exec('scheduling_started', {"number_of_tasks":len(self.not_completed)})
         # Check that dependencies are OK
-        
-        # Wait till all processes are available
+
+        # Wait till all processes are available (the scheduling usually happens in an already set up MPI
+        # environment, so we have to ensure that everything syncs before).
         self.comm.Barrier()
+
         # Execute all tasks
         # rank 0 is the controller
-        
+
         busy_processes = [False]*(self.number_of_processes)
         busy_processes[0] = True
-#         if self.rank == 0:
-#             # controller
-#             print "I am the 0th, so the controller"
-#         else:
-#             # task executing
-#             print "I am a slave", self.rank
-        
+
         available_workers = self.number_of_processes - 1
         while not len(self.finished) == len(self.tasks):
+            #
+            # 'MASTER' PROCESS
+            #
             if self.rank == 0:
                 # controller
                 cannot_choose_a_task = False
@@ -55,14 +65,14 @@ class MPIParallelScheduler(SerialScheduler):
                     except:
                         pass
                     if available_process is not None:
-                        self.function_exec('task_started', {"task_name":task.name})
+                        self.function_exec('task_started', {"task_name":task_name})
                         self.comm.send(("EXECUTE",task_name), dest = available_process, tag=1)
                         self.lock_task(task_name) # from now on this task is not available for choosing
                         self.running.append(task_name)
                         busy_processes[available_process] = True
                 else:
                     cannot_choose_a_task = True
-                    
+
                 if cannot_choose_a_task or len(self.running) == available_workers:
                     # Wait for a result
                     ended_task_name, result, sender_rank = self.comm.recv(source = MPI.ANY_SOURCE, tag = 2)
@@ -72,6 +82,9 @@ class MPIParallelScheduler(SerialScheduler):
                     self.complete_task(ended_task_name)
                     self.remove_from_dependencies(ended_task_name)
                     busy_processes[sender_rank] = False
+            #
+            # 'SLAVE' PROCESS
+            #
             else:
                 # Task executing.
                 # Wait for a task number to be executed
@@ -83,19 +96,16 @@ class MPIParallelScheduler(SerialScheduler):
                     self.comm.send((task_name,result, self.rank), dest = 0, tag = 2)
                 elif message == "FINISH":
                     break # exit the loop
-        
-#         print self.rank, "reached main loop end"
 
         if self.rank == 0:
             for i in range(1,self.number_of_processes):
                 self.comm.send(("FINISH",None), dest = i, tag=1)
-        
+
         if self.share_results_with_all_processes:
             self.results = self.comm.bcast(self.results, root=0)
-            
+
         self.function_exec('scheduling_ended')
 
         return self.results
-        
-        
-        
+
+
